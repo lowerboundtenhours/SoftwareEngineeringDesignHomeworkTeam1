a02373fa1da5b0f9f1e02d57a47291da570ef766
Tony Liang <b04901019@ntu.edu.tw>
2019-05-25 11:42:28 +0800

    add WindowFramework

---
+++ WindowFramework/Team1/XWindow.java
@@ -0,0 +1,9 @@
+public class XWindow implements Implement {
+    public void drawRect() {
+        System.out.println("XXXX");
+    }
+
+    public void drawText() {
+        System.out.println("XWindow");
+    }
+}
